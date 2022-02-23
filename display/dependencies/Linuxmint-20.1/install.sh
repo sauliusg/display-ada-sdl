823e7ebb8746053590fe7898692dc6fb7443e540
saulius <saulius@6d732be0-bcae-43da-b8eb-f61bb99a8e0a>
2022-02-23 11:54:25 +0000

    ada/ (saulius@tasmanijos-velnias) Installing also 'libsdl1.2-dev'.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/display-ada-sdl/trunk@6 6d732be0-bcae-43da-b8eb-f61bb99a8e0a

--- display/dependencies/Linuxmint-20.1/install.sh
+++ display/dependencies/Linuxmint-20.1/install.sh
@@ -3,6 +3,6 @@
 # INstall dependencies for this project:
 
 sudo apt-get install -y libglu1-mesa-dev freeglut3-dev 
-sudo apt-get install -y libsdl2-dev
+sudo apt-get install -y libsdl1.2-dev
 sudo apt-get install -y libsdl2-dev
 sudo apt-get install -y libsdl2-ttf-dev
